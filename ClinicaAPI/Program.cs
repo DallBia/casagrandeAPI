d22f41a556197be8a1fcbf46c781fb3ca83bcdaa
DallBia <146030584+DallBia@users.noreply.github.com>
2024-01-08 19:24:00 -0300

    Update Program.cs

--- ClinicaAPI/Program.cs
+++ ClinicaAPI/Program.cs
@@ -81,7 +81,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowMyOrigin",
         
         //builder => builder.WithOrigins("http://localhost:4200")
-        builder => builder.WithOrigins("http://34.66.160.170")
+        builder => builder.WithOrigins("http://34.123.211.220")
                           .AllowAnyMethod()
                           .AllowAnyHeader());
 });
