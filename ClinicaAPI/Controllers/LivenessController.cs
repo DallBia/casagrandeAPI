f08371f94dabb346e9a284e7dff918df012daaa6
Bi-Dall <bianchini.dallacqua@gmail.com>
2024-01-19 00:11:09 -0300

    testests

---
+++ ClinicaAPI/Controllers/LivenessController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicaAPI.Controllers
+{
+    [ApiController]
+    [Route("")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class LivenessController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
